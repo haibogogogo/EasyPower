b758b48e03e6e2c8e2bf2fa7935f2d53e18c00ed
haibogogogo <70358584+haibogogogo@users.noreply.github.com>
2020-09-03 11:53:03 +0800

    tool
    srecordizer tool ,which can analysis srec

--- cm.app.usr.xPower/src/boardInfo.c
+++ cm.app.usr.xPower/src/boardInfo.c
@@ -47,4 +47,93 @@ const uint8_t* BSP_GetBoardID(void)
     return (const uint8_t*)STM32G4XX_NUCLEO_BSP_BOARD_ID;
 }
 
+/***********************************************************************************************************************
+ *  SoftwareVersion_GetBootSWVerID
+ **********************************************************************************************************************/
+/*! \brief         This function shall be called to get VIP Bootloader Version ID.
+ *
+ *  \param[in,out] pbDiagData : Pointer to the request buffer
+ *  \return        void.
+ **********************************************************************************************************************/
+FUNC(void, SWVERSION_CODE) SoftwareVersion_GetBootSWVerID(uint8* buffer)
+{
+    uint8 index = 0x00u;
+    const uint8* pcu8_VipBootloaderVersionId = ((const uint8*)&__rom_bl_version_id);
+
+    if(NULL != buffer)
+    {
+        for(index = 0x00u ; index < SW_VERSION_ID_SIZE ; index++)
+        {
+            buffer[index] = pcu8_VipBootloaderVersionId[(SW_VERSION_ID_SIZE - 0x01u) - index];
+        }
+    }
+}
+
+/***********************************************************************************************************************
+ *  SoftwareVersion_GetBootSWVerString
+ **********************************************************************************************************************/
+/*! \brief         This function shall be called to get VIP Bootloader Version String.
+ *
+ *  \param[in,out] pbDiagData : Pointer to the request data
+ *  \param[in]     diagReqDataLen : Length of request data
+ *  \return        void.
+ **********************************************************************************************************************/
+FUNC(void, SWVERSION_CODE) SoftwareVersion_GetBootSWVerString(uint8* buffer)
+{
+    uint8 index = 0x00u;
+    const uint8* pcu8_VipBootloaderVersionString = ((const uint8*)&__rom_bl_version_string);
+
+    if(NULL != buffer)
+    {
+        for(index = 0x00u ; index < SW_VERSION_STRING_SIZE ; index++)
+        {
+            buffer[index] = pcu8_VipBootloaderVersionString[index];
+        }
+    }
+}
+
+/***********************************************************************************************************************
+ *  SoftwareVersion_GetApplSWVerID
+ **********************************************************************************************************************/
+/*! \brief         This function shall be called to get VIP Bootloader Version ID.
+ *
+ *  \param[in,out] pbDiagData : Pointer to the request buffer
+ *  \return        void.
+ **********************************************************************************************************************/
+FUNC(void, SWVERSION_CODE) SoftwareVersion_GetApplSWVerID(uint8* buffer)
+{
+    uint8 index = 0x00u;
+    const uint8* pcu8_VipApplicationVersionId = ((const uint8*)&__rom_sw_version_id);
+
+    if(NULL != buffer)
+    {
+        for(index = 0x00u ; index < SW_VERSION_ID_SIZE ; index++)
+        {
+            buffer[index] = pcu8_VipApplicationVersionId[(SW_VERSION_ID_SIZE - 0x01u) - index];
+        }
+    }
+}
+
+/***********************************************************************************************************************
+ *  SoftwareVersion_GetApplSWVerString
+ **********************************************************************************************************************/
+/*! \brief         This function shall be called to get VIP Bootloader Version String.
+ *
+ *  \param[in,out] pbDiagData : Pointer to the request data
+ *  \param[in]     diagReqDataLen : Length of request data
+ *  \return        void.
+ **********************************************************************************************************************/
+FUNC(void, SWVERSION_CODE) SoftwareVersion_GetApplSWVerString(uint8* buffer)
+{
+    uint8 index = 0x00u;
+    const uint8* pcu8_VipApplicationVersionString = ((const uint8*)&__rom_sw_version_string);
+
+    if(NULL != buffer)
+    {
+        for(index = 0x00u ; index < SW_VERSION_STRING_SIZE ; index++)
+        {
+            buffer[index] = pcu8_VipApplicationVersionString[index];
+        }
+    }
+}
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
